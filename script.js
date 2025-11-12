1e53f1604bf8cd2d98583acf0a4f1aae8076d3c3
igorivor <igorrako69@gmail.com>
2025-11-12 17:19:48 +0100

    Third phase of project implementing playRound function to determine winner of round modified getHumanChoice and getCamputerChoice to return value instead of loging value adding to global variables humanScore and computerScore

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getCamputerChoice(){
     let percentage = 0;
     percentage = Math.random() * 100;
     if(percentage >= 0 && percentage <= 32.99){
-        console.log("Rock!");
+        return "Rock";
     } else if(percentage >= 33 && percentage <= 66.99){
-        console.log("Paper!");
+       return "Paper";
     } else if(percentage >= 67 && percentage <= 100){
-        console.log("Scissors!");
+        return "Scissors";
     }
 }
 
 function getHumanChoice(){
     let tip = prompt("Select: Rock! Paper! or Scissors!");
     if(tip.toLowerCase() === "rock"){
-        console.log("Rock!");
+        return "Rock";
     }else if(tip.toLowerCase() === "paper"){
-        console.log("Paper!");
+        return "Paper";
     }else if(tip.toLowerCase() === "scissors"){
-        console.log("Scissors!");
+        return "Scissors";
     }
 }
 
-getHumanChoice();
-getCamputerChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getCamputerChoice();
+
+function playRound(humanSelection, computerSelection){
+
+    if(humanSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock" ){
+        console.log("Tie! Same selection!");
+    }else if(humanSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper" ){
+        console.log("Computer Victory! Paper beats Rock!");
+        computerScore++;
+    }else if(humanSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors" ){
+        console.log("Human Victory! Rock beats Scissors!");
+        humanScore++;
+    }else if(humanSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper" ){
+        console.log("Tie! Same selection!");
+    }else if(humanSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock" ){
+        console.log("Human Victory! Paper beats Rock!");
+        humanScore++;
+    }else if(humanSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors" ){
+        console.log("Computer Victory! Scissors beats Paper!");
+        computerScore++;
+    }else if(humanSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors" ){
+        console.log("Tie! Same selection!");
+    }else if(humanSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper" ){
+        console.log("Human Victory! Scissors beats Paper!");
+        humanScore++;
+    }else if(humanSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock" ){
+        console.log("Computer Victory! Rock beats Scissors!");
+        computerScore++;
+    }
+}
+
+playRound(humanSelection,computerSelection);
