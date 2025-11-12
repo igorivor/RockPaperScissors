507c97ce330e435025432a50da50dfd5463b828d
igorivor <igorrako69@gmail.com>
2025-11-12 18:19:15 +0100

    Final phase of project implementing playGame function to play game of 5 rounds to detrdeterminewinner of game improved function playRound to return value based on winner of set round

--- script.js
+++ script.js
@@ -24,36 +24,60 @@ function getHumanChoice(){
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getCamputerChoice();
-
 function playRound(humanSelection, computerSelection){
 
     if(humanSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock" ){
         console.log("Tie! Same selection!");
+        return -1;
     }else if(humanSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper" ){
         console.log("Computer Victory! Paper beats Rock!");
-        computerScore++;
+        return 0;
     }else if(humanSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors" ){
         console.log("Human Victory! Rock beats Scissors!");
-        humanScore++;
+        return 1;
     }else if(humanSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper" ){
         console.log("Tie! Same selection!");
+        return -1;
     }else if(humanSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock" ){
         console.log("Human Victory! Paper beats Rock!");
-        humanScore++;
+        return 1;
     }else if(humanSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors" ){
         console.log("Computer Victory! Scissors beats Paper!");
-        computerScore++;
+        return 0;
     }else if(humanSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors" ){
         console.log("Tie! Same selection!");
+        return -1;
     }else if(humanSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper" ){
         console.log("Human Victory! Scissors beats Paper!");
-        humanScore++;
+        return 1;
     }else if(humanSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock" ){
         console.log("Computer Victory! Rock beats Scissors!");
-        computerScore++;
+        return 0;
     }
 }
+function playGame(){
+    let score = 0;
+    for(let i = 0; i < 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getCamputerChoice();
+        score = playRound(humanSelection,computerSelection);
+        if(score == 0){
+            computerScore++;
+        }else if(score == 1){
+            humanScore++;
+        }else if(score == -1){
+            i--;
+        }else{
+            console.log("Error!");
+        }
+    }
+    if(humanScore > computerScore){
+        console.log("Human Won Game! Score was:" +humanScore +" :" +computerScore +"!");
+    }else if(computerScore > humanScore){
+        console.log("Computer Won Game! Score was: " +computerScore +":" +humanScore +"!");
+    }else {
+        console.log("Error!");
+    }
+}
+playGame();
 
-playRound(humanSelection,computerSelection);
