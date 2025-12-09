176f4f6592c1176f85a3fc1855f8de805b12afa9
igorivor <igorrako69@gmail.com>
2025-12-09 16:20:03 +0100

    Changed code in files index.html and script.js to move game form terminal to webpage. Add buttons for rock, paper,scissor and modified logic for game!

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let humanScore = 0;
-let computerScore = 0;
+let computerScore = 0; 
+let tie = 0;
 
-function getCamputerChoice(){
+const btn_Rock = document.querySelector("#btn_Rock");
+const btn_Paper = document.querySelector("#btn_Paper");
+const btn_Scissors = document.querySelector("#btn_Scissors");
+const msg_Resualt = document.querySelector(".resualts")
+
+btn_Rock.addEventListener("click",() => playGame("rock"));
+btn_Paper.addEventListener("click",() => playGame("paper"));
+btn_Scissors.addEventListener("click",() => playGame("scissors"));
+
+function getComputerChoice(){
     let percentage = 0;
     percentage = Math.random() * 100;
     if(percentage >= 0 && percentage <= 32.99){
@@ -13,71 +23,80 @@ function getCamputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let tip = prompt("Select: Rock! Paper! or Scissors!");
-    if(tip.toLowerCase() === "rock"){
-        return "Rock";
-    }else if(tip.toLowerCase() === "paper"){
-        return "Paper";
-    }else if(tip.toLowerCase() === "scissors"){
-        return "Scissors";
-    }
-}
-
 function playRound(humanSelection, computerSelection){
-
+    const prf_Round = document.createElement("p")
     if(humanSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock" ){
-        console.log("Tie! Same selection!");
+        prf_Round.textContent = "Tie! Same selection!";
+        msg_Resualt.appendChild(prf_Round);
         return -1;
     }else if(humanSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper" ){
-        console.log("Computer Victory! Paper beats Rock!");
+        prf_Round.textContent = "Computer Victory! Paper beats Rock!";
+        msg_Resualt.appendChild(prf_Round);
         return 0;
     }else if(humanSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors" ){
-        console.log("Human Victory! Rock beats Scissors!");
+        prf_Round.textContent = "Human Victory! Rock beats Scissors!";
+        msg_Resualt.appendChild(prf_Round);
         return 1;
     }else if(humanSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper" ){
-        console.log("Tie! Same selection!");
+        prf_Round.textContent = "Tie! Same selection!";
+        msg_Resualt.appendChild(prf_Round);
         return -1;
     }else if(humanSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock" ){
-        console.log("Human Victory! Paper beats Rock!");
+        prf_Round.textContent = "Human Victory! Paper beats Rock!";
+        msg_Resualt.appendChild(prf_Round);
         return 1;
     }else if(humanSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors" ){
-        console.log("Computer Victory! Scissors beats Paper!");
+        prf_Round.textContent = "Computer Victory! Scissors beats Paper!";
+        msg_Resualt.appendChild(prf_Round);
         return 0;
     }else if(humanSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors" ){
-        console.log("Tie! Same selection!");
+        prf_Round.textContent = "Tie! Same selection!";
+        msg_Resualt.appendChild(prf_Round);
         return -1;
     }else if(humanSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper" ){
-        console.log("Human Victory! Scissors beats Paper!");
+        prf_Round.textContent = "Human Victory! Scissors beats Paper!";
+        msg_Resualt.appendChild(prf_Round);
         return 1;
     }else if(humanSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock" ){
-        console.log("Computer Victory! Rock beats Scissors!");
+        prf_Round.textContent = "Computer Victory! Rock beats Scissors!";
+        msg_Resualt.appendChild(prf_Round);
         return 0;
     }
 }
-function playGame(){
-    let score = 0;
-    for(let i = 0; i < 5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getCamputerChoice();
-        score = playRound(humanSelection,computerSelection);
-        if(score == 0){
-            computerScore++;
-        }else if(score == 1){
-            humanScore++;
-        }else if(score == -1){
-            i--;
-        }else{
-            console.log("Error!");
-        }
-    }
-    if(humanScore > computerScore){
-        console.log("Human Won Game! Score was:" +humanScore +" :" +computerScore +"!");
-    }else if(computerScore > humanScore){
-        console.log("Computer Won Game! Score was: " +computerScore +":" +humanScore +"!");
-    }else {
+
+function playGame(humanChoice){
+
+    const prf_Game = document.createElement("p");
+    const humanSelection = humanChoice;
+    const computerSelection = getComputerChoice();
+    let score = playRound(humanSelection,computerSelection); 
+
+    if(score == 0){
+        computerScore++;
+    }else if(score == 1){
+        humanScore++;
+    }else if(score == -1){
+        tie++;
+    }else{
         console.log("Error!");
+    } 
+
+   
+    if(humanScore === 5){
+        const prf_Game = document.createElement("p");
+        prf_Game.textContent = `Human Won Game! Score: ${humanScore}:${computerScore} with ${tie} ties!`;
+        msg_Resualt.appendChild(prf_Game);
+        humanScore = 0;
+        computerScore = 0;
+        tie = 0;
+    }else if(computerScore === 5){
+        const prf_Game = document.createElement("p");
+        prf_Game.textContent = `Computer Won Game! Score: ${computerScore}:${humanScore} with ${tie} ties!`;
+        msg_Resualt.appendChild(prf_Game);
+        humanScore = 0;
+        computerScore = 0;
+        tie = 0;
     }
 }
-playGame();
+
 
